4c252cded62dade392c391b9a7871eefc4a1208c
Santiago-Pe <penasantiago364@gmail.com>
2023-04-27 14:36:21 -0300

    starting to use emotion styles

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,2 +1,37 @@
 /* ------------ Dependecies ------------*/
 import React from "react";
+// import AppBar from '@mui/material/AppBar';
+// import Typography from '@mui/material/Typography';
+// import Toolbar from '@mui/material/Toolbar';
+// import IconButton from '@mui/material/IconButton';
+// import SearchIcon from '@mui/icons-material/Search';
+// import InputBase from '@mui/material/InputBase';
+// import Switch from '@mui/material/Switch';
+// import { withStyles } from "@mui/material";
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+  color: ${props => (props.primary ? 'hotpink' : 'turquoise')};
+`
+const Container = styled.div(props => ({
+    display: 'flex',
+    flexDirection: props.column && 'column'
+  }))
+/* ------------ Component ------------*/
+
+const Navbar = () => {
+
+    /* ------------ Render ------------*/
+
+    return(
+
+        <Container column>
+            <Button>This is a regular button.</Button>
+            <Button primary>This is a primary button.</Button>
+        </Container>
+    )
+
+
+}
+
+export default Navbar;
